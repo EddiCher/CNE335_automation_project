4bd38468838da427449a3bd518d06f9b7ee4bd37
EddiCher <103545139+EddiCher@users.noreply.github.com>
2022-12-11 21:51:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # Justin Ellis
 # CNE 335 Fall
 
+import Server
+
 def print_program_info():
     # TODO - Change your name
     print("Server Automator v0.1 by Your Name")
@@ -9,5 +11,6 @@ def print_program_info():
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+    #The server.Server("IP ADDRESS GOES HERE") in this case EC2 IP is 52.14.192.27
+    #The IP address is passed as a string to the Server class in the server.py module
+    ec2Server = Server.Server("52.26.15.148")
